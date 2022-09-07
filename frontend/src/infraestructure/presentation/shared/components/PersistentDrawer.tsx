3df5a091410cff588308e05ec27e0eba04074d9b
nullpointer-excelsior <red.tiger.developer@proton.me>
2022-09-07 15:04:00 -0400

    button to open neo4j browser added on drawer menu

--- frontend/src/infraestructure/presentation/shared/components/PersistentDrawer.tsx
+++ frontend/src/infraestructure/presentation/shared/components/PersistentDrawer.tsx
@@ -23,7 +23,8 @@ import DoubleArrowIcon from '@mui/icons-material/DoubleArrow';
 import { RelationshipForm } from '../../pages/neo4j-maintainer/components/RelationshipForm';
 import EntityForm from '../../pages/neo4j-maintainer/components/Neo4jNodeForm';
 import { useUiState } from '../../../state/hooks/useUiState';
-import { Container } from '@mui/material';
+import { Button, Container, SvgIcon } from '@mui/material';
+import DoneIcon from '@mui/icons-material/Done';
 
 const drawerWidth = 240;
 
@@ -108,6 +109,10 @@ export default function PersistentDrawer() {
     setOpen(false);
   };
 
+  const openNeo4jBrowser = () => {
+    window.open('http://localhost:7474/')
+  }
+
   const contentSwitch: any = {
     'create-node': <EntityForm />,
     'create-relationship': <RelationshipForm />,
@@ -191,6 +196,12 @@ export default function PersistentDrawer() {
             </ListItemButton>
           </ListItem>
         </List>
+        <Divider />
+        <List>
+          <Box display="flex" justifyContent="center" alignItems="center">
+            <Button onClick={() =>openNeo4jBrowser()} size="small" variant="contained" startIcon={<DatabaseIcon />} >ABRIR NEO4J BROWSER</Button>
+          </Box>
+        </List>
       </Drawer>
       <Main open={open}>
         <DrawerHeader />
@@ -203,3 +214,22 @@ export default function PersistentDrawer() {
 }
 
 
+function DatabaseIcon({ ...props}) {
+  return (
+    <SvgIcon  viewBox="0 0 29.26 29.26" {...props}>
+      <g>
+        <path style={{ fill:"#030104;"}} d="M14.627,15.954c-5.494,0-10.098-1.614-11.355-3.79c-0.221,0.383-0.35,0.781-0.35,1.194v3.561
+          c0,2.751,5.242,4.983,11.705,4.983c6.467,0,11.711-2.232,11.711-4.983v-3.561c0-0.413-0.131-0.811-0.354-1.194
+          C24.727,14.339,20.127,15.954,14.627,15.954z"/>
+        <path style={{ fill:"#030104;"}} d="M14.627,23.31c-5.494,0-10.098-1.616-11.355-3.788c-0.221,0.381-0.35,0.779-0.35,1.191v3.564
+          c0,2.752,5.242,4.983,11.705,4.983c6.467,0,11.711-2.23,11.711-4.983v-3.564c0-0.412-0.131-0.81-0.354-1.19
+          C24.727,21.694,20.127,23.31,14.627,23.31z"/>
+        <path style={{ fill:"#030104;"}} d="M26.018,5.042c-0.23,2.016-5.232,3.629-11.391,3.629c-6.156,0-11.158-1.613-11.387-3.628
+          C3.039,5.409,2.922,5.791,2.922,6.184v3.561c0,2.752,5.242,4.983,11.705,4.983c6.467,0,11.711-2.23,11.711-4.983V6.184
+          C26.338,5.791,26.219,5.409,26.018,5.042z"/>
+        <path style={{ fill:"#030104;"}} d="M14.627,7.541c6.303,0,11.41-1.687,11.41-3.771c0-2.082-5.107-3.77-11.41-3.77
+          C8.328,0.001,3.219,1.689,3.219,3.771C3.219,5.854,8.328,7.541,14.627,7.541z"/>
+      </g>
+    </SvgIcon>
+  );
+}
